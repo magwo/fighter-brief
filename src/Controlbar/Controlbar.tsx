ecc50fd2f7225411ba69b54bfcb35d0ac682719e
Magnus Wolffelt <magnus.wolffelt@avanza.se>
2022-07-28 17:34:20 +0200

    Loop and clickable timebar

--- src/Controlbar/Controlbar.tsx
+++ src/Controlbar/Controlbar.tsx
@@ -2,6 +2,8 @@ import React, { FC, useState } from 'react';
 import useAnimationFrame from '../useAnimationFrame';
 import './Controlbar.css';
 
+const TIME_BAR_WIDTH = 400;
+
 interface ControlbarProps {
   onTimeChange: (timeDelta: number, time: number) => void,
   onPlayPause: (shouldPlay: boolean) => void
@@ -10,10 +12,15 @@ interface ControlbarProps {
 const Controlbar: FC<ControlbarProps> = (props: ControlbarProps) => {
   const [time, setTime] = useState<number>(0);
   const [shouldPlay, setShouldPlay] = useState<boolean>(false);
+  const [shouldLoop, setShouldLoop] = useState<boolean>(false);
 
   useAnimationFrame((_time: { time: number; delta: number }) => {
     const timeDelta = shouldPlay ? _time.delta : 0;
-    const newTime = time + timeDelta;
+    let newTime = time + timeDelta;
+    // TODO: Make maximum time adaptive based on units
+    if (newTime > 1.0 && shouldLoop) {
+      newTime = 0;
+    }
     setTime(newTime);
     props.onTimeChange(timeDelta, newTime);
   });
@@ -30,13 +37,36 @@ const Controlbar: FC<ControlbarProps> = (props: ControlbarProps) => {
     setShouldPlay(newPlayState);
   }
 
+  const toggleLoop = () => {
+    const newLoopState = !shouldLoop;
+    setShouldLoop(newLoopState);
+  }
+
+  const mouseMoveOnTimebar = (e: React.MouseEvent) => {
+    if (e.buttons === 1 && e.target === e.currentTarget) {
+      const relX = e.currentTarget.getBoundingClientRect().left;
+      // const relY = e.currentTarget.clientTop;
+      const fraction = (e.clientX - relX) / TIME_BAR_WIDTH;
+      setTime(fraction);
+      console.log("Moving on timebar", e.clientX);
+    }
+  }
+
+  const styleTimeHandle = {
+    transform: `translate(${time * TIME_BAR_WIDTH}px, 0)`
+  };
+
   return (
     <div className="Controlbar" data-testid="Controlbar">
       <div className="buttons">
         <button className='play-pause' onClick={() => { playPause() }}>{shouldPlay ? "⏸" : "▶"}</button>
-        <button className='rewind' onClick={() => rewind() }>⏪</button>
+        <button className='rewind' onClick={() => rewind()}>⏪</button>
+        <button className='loop' onClick={() => toggleLoop()}>{shouldLoop ? "Loop" : "Noloop"}</button>
       </div>
-    </div>
+      <div className="timebar" onMouseMove={(e: React.MouseEvent) => mouseMoveOnTimebar(e) } onMouseDown={(e: React.MouseEvent) => mouseMoveOnTimebar(e) }>
+        <div style={styleTimeHandle} className="handle"></div>
+      </div>
+    </div >
   );
 }
 
